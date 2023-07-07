36c08636ef9394877302fb6362d9caea758f04db
Artur Duarte <arturduartemoraes@gmail.com>
2023-07-07 14:59:11 -0300

    ✨ feat: add example of spread and rest operator

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 const age = null;
 console.log('Sua idade é: ' + (age ?? 'Não informado'));
 
-// Objetos
+// Objects
 const user = {
 	name: 'Artur',
 	nickname: 'Moraes',
@@ -19,7 +19,7 @@ console.log(Object.values(user));
 console.log(JSON.stringify(Object.values(user)));
 console.log(JSON.stringify(Object.entries(user)));
 
-// Desestruturação
+// Destructuring
 function showAge({ age }) {
 	return age;
 }
@@ -28,3 +28,19 @@ console.log(showAge(user));
 
 const { address: endereco, age: idade, nickname = 'Duarte' } = user;
 console.log(JSON.stringify({ endereco, idade, nickname }));
+
+// Spread operator
+const newUser = { ...user, city: 'São Paulo' };
+console.log(newUser);
+
+const { name, ...rest } = user;
+console.log(JSON.stringify(rest));
+
+// Rest operator
+function myFunc(a, b, ...otherArgs) {
+	console.log(a); // 1
+	console.log(b); // 2
+	console.log(otherArgs); // [3, 4, 5]
+}
+
+myFunc(1, 2, 3, 4, 5);
