250ab0eb0842d5a91b71af21fe2ca12d7b51ebbf
Artur Duarte <arturduartemoraes@gmail.com>
2023-07-07 12:31:45 -0300

    ✨ feat: add example of objects and methods

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
 // Nullish coalescing operator
-
 const idade = null;
+console.log('Sua idade é: ' + (idade ?? 'Não informado'));
 
-document.body.innerHTML = 'Sua idade é: ' + (idade ?? 'Não informado');
+// Objetos
+const user = {
+	name: 'Artur',
+	age: 18,
+	adress: {
+		street: 'Rua Três',
+		number: 53,
+	},
+};
+
+console.log('name' in user);
+console.log(Object.keys(user));
+console.log(Object.values(user));
+console.log(JSON.stringify(Object.values(user)));
+console.log(JSON.stringify(Object.entries(user)));
