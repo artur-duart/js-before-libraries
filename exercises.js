8a208e32f605e63a26dea500771cc837649b5a4a
Artur Duarte <arturduartemoraes@gmail.com>
2023-07-15 23:47:49 -0300

    ✨ feat: add exercises

---
+++ exercises.js
@@ -0,0 +1,15 @@
+// 1. Using the Nullish Coalescing Operator: Write a function that takes an object as an argument and returns the value of the name property if it exists, or the string 'Unknown' if it doesn't. Use the Nullish Coalescing Operator to achieve this.
+
+// 2. Destructuring and Template Literals: Write a function that takes an object representing a person as an argument and returns a string with a greeting message using the person's name and age. Use destructuring to extract the values from the object and Template Literals to create the greeting message.
+
+// 3. Spread Operator and Rest Operator: Write a function that takes an indefinite number of arrays as arguments and returns a new array containing all the elements from all the arrays, without any duplicates. Use the Spread Operator and Rest Operator to achieve this.
+
+// 4. Filter and Map: Write a function that takes an array of numbers as an argument and returns a new array containing only the even numbers from the original array, but multiplied by 10. Use the filter and map methods to achieve this.
+
+// 5. Every and Some: Write two functions: one that takes an array of numbers as an argument and returns true if all the numbers are positive, and another that takes an array of numbers as an argument and returns true if at least one of the numbers is positive. Use the every and some methods to achieve this.
+
+// 6. Find and Find Index: Write two functions: one that takes an array of numbers as an argument and returns the first even number in the array, and another that takes an array of numbers as an argument and returns the index of the first even number in the array. Use the find and findIndex methods to achieve this.
+
+// 7. Reduce: Write a function that takes an array of numbers as an argument and returns the sum of all the numbers in the array. Use the reduce method to achieve this.
+
+// 8. Promises: Write a function that uses fetch to send a request to an API (e.g., https://jsonplaceholder.typicode.com/users) and returns a Promise that resolves with the data from the response. Use Promises to handle asynchronous operations.
--- main.js
+++
@@ -1,3 +0,0 @@
-import { sum } from './lib/math.js';
-
-console.log(sum(2, 3))
